cb97469f1b68d6c613d26d6b38a8f677d8dd4d12
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-04-20 11:51:58 -0400

    flash card model

---
+++ FlashCards/Models/FlashCard.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlashCards.Models
+{
+    public class FlashCard
+    {
+        public int Id { get; set; }
+
+        public string Question {get; set;}
+        public string Answer {get; set;}
+    }
+}
