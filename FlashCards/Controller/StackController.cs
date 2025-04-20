689798efe0595cddbf18294079ceae05dbf1b7d6
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-04-20 12:06:47 -0400

    stack controller skeleton

--- FlashCards/Controller/StackController.cs
+++ FlashCards/Controller/StackController.cs
@@ -7,6 +7,25 @@ namespace FlashCards.Controller
 {
     public class StackController
     {
-        
+
+        public void InsertToStack()
+        {
+
+        }
+
+        public void DeleteFromStack()
+        {
+
+        }
+
+        public void UpdateStack()
+        {
+            
+        }
+
+        public void DisplayAllStacks()
+        {
+
+        }
     }
 }
