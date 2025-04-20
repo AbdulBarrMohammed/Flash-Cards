3cf6a2dbc2756549bbb03a37809175f5a8c0c006
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-04-20 12:00:36 -0400

    study session model

---
+++ FlashCards/Models/StudySession.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlashCards.Models
+{
+    public class StudySession
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public int Score { get; set; }
+    }
+}
--- FlashCards/obj/Debug/net9.0/FlashCards.AssemblyInfo.cs
+++ FlashCards/obj/Debug/net9.0/FlashCards.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlashCards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb97469f1b68d6c613d26d6b38a8f677d8dd4d12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910b1ac8586ec2c51d881cc34019df1b99ecc27b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlashCards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlashCards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
